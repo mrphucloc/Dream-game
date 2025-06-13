c7e201674d800507a57abece606a083e645580f0
mrphucloc <phucloctwe1234@gmail.com>
2025-06-13 20:10:34 +0700

    pool

---
+++ Assets/Scriit/ObjcetPool.cs
@@ -0,0 +1,15 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ObjcetPool : MonoBehaviour
+{
+    
+    [SerializeField] public struct PooledObject { 
+        
+
+    }
+    public void init() { 
+    
+    }
+    
+}
