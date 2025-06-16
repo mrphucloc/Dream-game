42037cb8be71d01f7b0a8e44a008f61ec6f2940b
mrphucloc <phucloctwe1234@gmail.com>
2025-06-16 18:25:25 +0700

    health bar

---
+++ Assets/Scriit/HealthService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace View
+{
+    public class HealthService
+    {
+        public HealthService()
+        {
+        }
+        public static object Instance { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
