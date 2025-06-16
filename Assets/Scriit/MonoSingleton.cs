50faaeeb8bc2a542fe8141978186670922d876c7
mrphucloc <phucloctwe1234@gmail.com>
2025-06-16 18:25:52 +0700

    monosing

---
+++ Assets/Scriit/MonoSingleton.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class MonoSingleton<T> : MonoBehaviour
+{
+    public static T Instance { get; private set; }
+
+   
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = (T)(object)this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else {
+            Debug.LogWarning($"An instance of {typeof(T).Name} already exists. Destroying duplicate.");
+            Destroy(gameObject);
+        }
+    }
+}
