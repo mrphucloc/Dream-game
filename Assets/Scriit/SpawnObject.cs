0940417b67a648d8e15e23d11c174bc198b1c055
mrphucloc <phucloctwe1234@gmail.com>
2025-06-21 13:22:54 +0700

    1

---
+++ Assets/Scriit/SpawnObject.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour
+{
+    [SerializeField] private ObjectPool objectPool;
+    [SerializeField] private string pooledObjectId;
+    private float currentX = 0;
+    void Start()
+    {
+        objectPool = ObjectPool.Instance;
+    }
+    public void Spawn(string pooledObjectId)
+    {
+        this.pooledObjectId = pooledObjectId;
+        var go = objectPool.GetObject(pooledObjectId);
+        if (go != null)
+        {
+            go.transform.position = new Vector3(currentX,0,0);
+            go.transform.rotation = Quaternion.identity;
+            currentX++;
+
+            StartCoroutine(ReturnObject(go));
+        }
+        else {
+            Debug.LogWarning($"No object found with ID: {pooledObjectId}");
+        }
+        
+    }
+    private IEnumerator ReturnObject(GameObject go)
+    {
+        yield return new WaitForSeconds(1f);
+       objectPool.ReturnObject(pooledObjectId, go);
+        currentX = 0;
+    }
+    private void OnGUI()
+    {
+        if(GUILayout.Button("Spawn Object"))
+        {
+           Spawn(pooledObjectId);
+        }
+    }
+}
