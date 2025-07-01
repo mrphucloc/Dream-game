d43340284d0a3903ecde57cbca90ac6b2c01df60
mrphucloc <phucloctwe1234@gmail.com>
2025-07-01 08:19:50 +0700

    CarAI

--- Assets/Scriit/CarController.cs
+++ Assets/Scriit/CarController.cs
@@ -14,11 +14,11 @@ namespace DefaultNamespace
             RearRight
         }
 
-        [Serializable]
+        [Serializable] 
         public struct Wheel
         {
             public Transform wheellTransform;
-            public WheelCollider wheelColliider;
+            public WheelCollider wheellCollider;
             public WheelType wheelType;
             public float MaxSteerAngle;
             public float MotorTorque;
@@ -27,63 +27,70 @@ namespace DefaultNamespace
         [SerializeField] private List<Wheel> wheelss;
 
         [SerializeField] private float MaxSpeed = 100f;
-        [SerializeField] private float acceleratiion = 10f;
+        [SerializeField] private float Acceleration = 10f;
         [SerializeField] private float BrakeForce = 50f;
+
+        [SerializeField] private bool canControl;
 
         private float currenttSpeed = 0f;
 
         private void Update()
         {
-            this.HandleeInput();
-            this.UpdateWheeels();
+            this.HandleInputt();
+            this.UpdateWheelss();
         }
 
-        private void UpdateWheeels()
+        private void UpdateWheelss()
         {
             foreach (var wheel in wheelss)
             {
                 // Update wheel rotation
                 float rotationAngle = currenttSpeed * Time.deltaTime;
-                wheel.wheellTransform.Rotate(Vector3.right, rotationAngle);
+                wheel.wheelTransform.Rotate(Vector3.right, rotationAngle);
 
                 // Update wheel position
                 Vector3 position;
                 Quaternion rotation;
-                wheel.wheelColliider.GetWorldPose(out position, out rotation);
+                wheel.wheellCollider.GetWorldPose(out position, out rotation);
                 wheel.wheellTransform.position = position;
                 wheel.wheellTransform.rotation = rotation;
             }
         }
 
-        private void HandleeInput()
+        public void ApplyInput(float horizontalInput, float verticalInput)
         {
-            float horizontalInput = Input.GetAxis("Horizontal");
-            float verticalInput = Input.GetAxis("Vertical");
-
             // Steer front wheels
             foreach (var wheel in wheelss)
             {
-                wheel.wheelColliider.steerAngle = horizontalInput * wheel.MaxSteerAngle;
-                float speed = verticalInput * wheel.MotorTorque * Time.deltaTime * acceleratiion;
+                wheel.wheellCollider.steerAngle = horizontalInput * wheel.MaxSteerAngle;
+                float speed = verticalInput * wheel.MotorTorque * Time.deltaTime * Acceleration;
                 currenttSpeed += speed;
                 currenttSpeed = Mathf.Clamp(currenttSpeed, -10f, MaxSpeed);
-                wheel.wheelColliider.motorTorque = currenttSpeed;
+                wheel.wheellCollider.motorTorque = currenttSpeed;
             }
             // Apply brake force
             if (Input.GetKey(KeyCode.Space))
             {
                 foreach (var wheel in wheelss)
                 {
-                    wheel.wheelColliider.brakeTorque = BrakeForce;
+                    wheel.wheellCollider.brakeTorque = BrakeForce;
                 }
             }
             else
             {
                 foreach (var wheel in wheelss)
                 {
-                    wheel.wheelColliider.brakeTorque = 0f;
+                    wheel.wheellCollider.brakeTorque = 0f;
                 }
             }
+        }
+
+        private void HandleInputt()
+        {
+            if (!canControl) return;
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+            ApplyInput(horizontalInput, verticalInput);
         }
     }
 }
